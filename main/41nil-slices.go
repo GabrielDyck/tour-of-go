f663309cfc91fcd7d4438ce9a476e211f3010db8
Gabriel Fernando Dyck <gabriel.dyck@despegar.com>
2019-08-14 23:09:28 -0300

    wip

---
+++ main/41nil-slices.go
@@ -0,0 +1,17 @@
+package main
+
+/*
+Nil slices
+The zero value of a slice is nil.
+
+A nil slice has a length and capacity of 0 and has no underlying array.
+ */
+import "fmt"
+
+func main() {
+	var s []int
+	fmt.Println(s, len(s), cap(s))
+	if s == nil {
+		fmt.Println("nil!")
+	}
+}
